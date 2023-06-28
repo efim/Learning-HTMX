578c5d7d0543907e695205a61f2f445a219a805d
efim <efim.nefedov@nordigy.ru>
2023-06-28 08:55:01 +0000

    feat(12): adding style guide to config

--- 12-order-summary-component-thymeleaf/tailwind.config.js
+++ 12-order-summary-component-thymeleaf/tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./src/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'pale-blue': 'hsl(225, 100%, 94%)',
+        'bright-blue': 'hsl(245, 75%, 52%)',
+        'very-pale-blue': 'hsl(225, 100%, 98%)',
+        'desaturated-blue': 'hsl(224, 23%, 55%)',
+        'dark-blue': 'hsl(223, 47%, 23%)',
+      },
+      fontFamily: {
+        'sans': ['Red Hat Display', 'sans-serif'], // This will set Roboto as the default sans font
+      },
+      fontWeight: {
+        'thin': 100,
+        'normal': 400,
+        'semibold': 600,
+        'bold': 700,
+        'extrabold': 900,
+      }
+
+    },
   },
   plugins: [],
 }
