2dff41f428d85b5f2e5f1712ba22f351ad2a3296
efim <efim.nefedov@nordigy.ru>
2023-06-29 12:13:18 +0000

    feat(13): style guide into config

--- 13-testimonials-grid-section/tailwind.config.js
+++ 13-testimonials-grid-section/tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.html"],
+  content: ["./src/**/*.{html, scala}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'moderate-violet': 'hsl(263, 55%, 52%)',
+        'very-dark-grayish-blue': 'hsl(217, 19%, 35%)',
+        'very-dark-blackish-blue': 'hsl(219, 29%, 14%)',
+        'light-gray': 'hsl(0, 0%, 81%)',
+        'light-grayish-blue': 'hsl(210, 46%, 95%)',
+      },
+      fontFamily: {
+        'sans': ['Barlow Semi Condensed', 'sans-serif'], // This will set Roboto as the default sans font
+      },
+      fontWeight: {
+        'normal': 400,
+        'bold': 500,
+      }
+    },
   },
   plugins: [],
 }
