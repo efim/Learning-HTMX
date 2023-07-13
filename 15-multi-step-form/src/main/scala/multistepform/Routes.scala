03aa9f8b94d6cc265a6a5fccc7b7ab945f2eef6c
efim <efim.nefedov@nordigy.ru>
2023-07-13 18:31:30 +0000

    feat(15): associating session with root page

--- 15-multi-step-form/src/main/scala/multistepform/Routes.scala
+++ 15-multi-step-form/src/main/scala/multistepform/Routes.scala
@@ -3,6 +3,8 @@ package multistepform
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver
 import org.thymeleaf.TemplateEngine
 import org.thymeleaf.context.Context
+import cask.endpoints.ParamReader
+import java.util.UUID
 
 case class Routes()(implicit cc: castor.Context, log: cask.Logger)
     extends cask.Routes {
@@ -14,13 +16,35 @@ case class Routes()(implicit cc: castor.Context, log: cask.Logger)
   val templateEngine = new TemplateEngine()
   templateEngine.setTemplateResolver(templateResolver)
 
+  val sessoinCookieName = "sessionId"
+
   @cask.get("/")
-  def getIndex() = {
+  def getIndex(ctx: cask.Request) = {
+    val sessionCookie = ctx.cookies.get(sessoinCookieName)
+    lazy val newSessionCookies = sessionCookie match {
+      case None => Seq(cask.Cookie(sessoinCookieName, UUID.randomUUID().toString(), path = "/"))
+      case Some(_) => Seq.empty // don't set new cookies
+    }
+
+    println(s"getting cookie $sessionCookie will set new? ${newSessionCookies}")
+
     val context = new Context()
     val indexPage = templateEngine.process("index", context)
     cask.Response(
       indexPage,
-      headers = Seq("Content-Type" -> "text/html;charset=UTF-8")
+      headers = Seq("Content-Type" -> "text/html;charset=UTF-8"),
+      cookies = newSessionCookies
+    )
+  }
+
+  @cask.get("/force-new-session")
+  def forceNewSession() = {
+    val newSessionCookie = cask.Cookie(sessoinCookieName, UUID.randomUUID().toString(), path = "/")
+    println(s"setting new session ${newSessionCookie.value}")
+    cask.Response(
+      s"New session forced. Force new session",
+      headers = Seq("Content-Type" -> "text/html;charset=UTF-8"),
+      cookies = Seq(newSessionCookie)
     )
   }
 
