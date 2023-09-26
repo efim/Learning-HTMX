51319b036bf60b92fa6d2428d41c0977fdef9b19
efim <efim666@bk.ru>
2023-09-26 15:21:03 +0000

    feat: dark theme, font icons
    courtesy of https://github.com/tailwindlabs/heroicons

--- 16-countries-page-from-api/tailwind.config.js
+++ 16-countries-page-from-api/tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/main/resources/**/*.html"],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
-        'dark-gray': 'hsl(0, 0%, 52%)',
-        'very-light-gray': 'hsl(0, 0%, 98%)',
+        'dark-gray': 'hsl(0, 0%, 52%)', // light mode input
+        'very-light-gray': 'hsl(0, 0%, 98%)', // light mode bg
+        'dark-blue': 'hsl(209, 23%, 22%)', // dark mode elements
+        'very-dark-blue': 'hsl(207, 26%, 17%)', // dark mode bg
       },
       fontFamily: {
         'sans': ['NunitoSans', 'sans-serif'],
