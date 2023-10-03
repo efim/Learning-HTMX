843e55841bd8f6ae77bf35bcbfa285ec799d0b45
efim <efim666@bk.ru>
2023-10-03 17:56:40 +0000

    fix: fontFamily override name

--- 17-results-summary-component-go/tailwind.config.js
+++ 17-results-summary-component-go/tailwind.config.js
@@ -10,14 +10,15 @@ module.exports = {
         'cobalt-blue': 'hsl(234, 85%, 45%)',
         'light-slate-blue': 'hsl(252, 100%, 67%)',
         'light-royal-blue': 'hsl(241, 81%, 54%)',
-        'violet-blue': 'hsla(256, 72%, 46%, 1)',
-        'persian-blue': 'hsla(241, 72%, 46%, 0)',
+        'violet-blue': 'hsla(256, 72%, 46%)',
+        'persian-blue': 'hsla(241, 72%, 46%)',
         'pale-blue': 'hsl(221, 100%, 96%)',
         'light-lavender': 'hsl(241, 100%, 89%)',
         'dark-gray-blue': 'hsl(224, 30%, 27%)',
       },
-      'font-family': {
+      'fontFamily': {
         'sans': ['HankenGrotesk', 'sans-serif'],
+        'added': ['HankenGrotesk', 'sans-serif'],
       },
     },
   },
