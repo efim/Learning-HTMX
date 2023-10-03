e834ff06ab975cc2bd92ea64ac1160ed08fb1ed7
efim <efim666@bk.ru>
2023-10-03 13:51:22 +0000

    feat: responding with a basic template

--- 17-results-summary-component-go/main.go
+++ 17-results-summary-component-go/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"embed"
+	"html/template"
 	"io"
 	"log"
 	"net/http"
@@ -14,8 +15,14 @@ var publicContent embed.FS
 func main() {
 	// visible on http://localhost:8080/static/public/some-text.txt
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.FS(publicContent))))
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+
+	http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
 		io.WriteString(w, "This is temporary here, hello")
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		tmpl := template.Must(template.ParseFiles("templates/summary-component.gohtml"))
+		tmpl.Execute(w, nil)
 	})
 
 	log.Print("starting server on default :8080")
