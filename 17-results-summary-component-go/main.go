b036002ca8c7221a06edf647cbb91c5c81b67af0
efim <efim666@bk.ru>
2023-10-04 06:03:04 +0000

    feat: repeating category in template

--- 17-results-summary-component-go/main.go
+++ 17-results-summary-component-go/main.go
@@ -14,6 +14,12 @@ var publicContent embed.FS
 //go:embed templates
 var templates embed.FS
 
+type Category struct {
+	Name     string
+	ColorHsl string
+	Score    int
+}
+
 // starts webserver that serves html page for exercise
 func main() {
 	// serves public static resources: bundled images, fonts, css
@@ -24,11 +30,20 @@ func main() {
 		io.WriteString(w, "This is temporary here, hello")
 	})
 
+	mySummary := [...]Category{
+		{Name: "Reaction", ColorHsl: "0deg 100% 67%", Score: 80},
+		{Name: "Memory", ColorHsl: "39deg 100% 56%", Score: 92},
+		{Name: "Verbal", ColorHsl: "166deg 100% 37%", Score: 61},
+		{Name: "Visual", ColorHsl: "234deg 85% 45%", Score: 72},
+	}
+
 	// main page with results summary
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		templateName :=  "templates/summary-component.gohtml"
 		tmpl := template.Must(template.ParseFS(templates, templateName))
-		tmpl.Execute(w, nil)
+		if err := tmpl.Execute(w, mySummary); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	})
 
 	log.Print("starting server on default :8080")
