133fa0df2b077d806fa54edfc1294e196a40d1e1
efim <efim666@bk.ru>
2023-10-04 05:15:53 +0000

    feat: colors for items via css variable

--- 17-results-summary-component-go/tailwind.config.js
+++ 17-results-summary-component-go/tailwind.config.js
@@ -4,10 +4,6 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'light-red': 'hsl(0, 100%, 67%)',
-        'orangey-yellow': 'hsl(39, 100%, 56%)',
-        'green-teal': 'hsl(166, 100%, 37%)',
-        'cobalt-blue': 'hsl(234, 85%, 45%)',
         'light-slate-blue': 'hsl(252, 100%, 67%)',
         'light-royal-blue': 'hsl(241, 81%, 54%)',
         'violet-blue': 'hsla(256, 72%, 46%)',
@@ -15,6 +11,7 @@ module.exports = {
         'pale-blue': 'hsl(221, 100%, 96%)',
         'light-lavender': 'hsl(241, 100%, 89%)',
         'dark-gray-blue': 'hsl(224, 30%, 27%)',
+        'summary-item-color': 'hsl(var(--summary-item-color-var) / <alpha-value>)',
       },
       'fontFamily': {
         'sans': ['HankenGrotesk', 'sans-serif'],
