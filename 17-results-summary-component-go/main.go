f4ab1ac7ec88ccf24bd1ecd93d2806768ca12333
efim <efim666@bk.ru>
2023-10-04 08:32:43 +0000

    feat: other fields taken from template data

--- 17-results-summary-component-go/main.go
+++ 17-results-summary-component-go/main.go
@@ -26,6 +26,12 @@ func iconPath(categoryName string) string {
 	return fmt.Sprintf("/static/public/images/icon-%s.svg", categoryName)
 }
 
+type ResultsSummaryData struct {
+	Categories   []Category
+	TotalScore   int
+	UpperPercent int
+}
+
 // starts webserver that serves html page for exercise
 func main() {
 	// serves public static resources: bundled images, fonts, css
@@ -36,22 +42,26 @@ func main() {
 		io.WriteString(w, "This is temporary here, hello")
 	})
 
-	mySummary := [...]Category{
-		{Name: "Reaction", ColorHsl: "0deg 100% 67%",
-			Score: 80, IconPath: iconPath("reaction")},
-		{Name: "Memory", ColorHsl: "39deg 100% 56%",
-			Score: 92, IconPath: iconPath("memory")},
-		{Name: "Verbal", ColorHsl: "166deg 100% 37%",
-			Score: 61, IconPath: iconPath("verbal")},
-		{Name: "Visual", ColorHsl: "234deg 85% 45%",
-			Score: 72, IconPath: iconPath("visual")},
+	resultsData := ResultsSummaryData{
+		Categories: []Category{
+			{Name: "Reaction", ColorHsl: "0deg 100% 67%",
+				Score: 80, IconPath: iconPath("reaction")},
+			{Name: "Memory", ColorHsl: "39deg 100% 56%",
+				Score: 92, IconPath: iconPath("memory")},
+			{Name: "Verbal", ColorHsl: "166deg 100% 37%",
+				Score: 61, IconPath: iconPath("verbal")},
+			{Name: "Visual", ColorHsl: "234deg 85% 45%",
+				Score: 72, IconPath: iconPath("visual")},
+		},
+		TotalScore:   76,
+		UpperPercent: 65,
 	}
 
 	// main page with results summary
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		templateName := "templates/summary-component.gohtml"
 		tmpl := template.Must(template.ParseFS(templates, templateName))
-		if err := tmpl.Execute(w, mySummary); err != nil {
+		if err := tmpl.Execute(w, resultsData); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
